8044faae81fd82e12719a0dbb8398510de6eb44b
tappiola <tatiana@util.co>
2025-08-10 19:14:42 +0100

    add pwa icons

--- apps/plain-chooser/public/sw.js
+++ apps/plain-chooser/public/sw.js
@@ -1,5 +1,14 @@
-const CACHE_NAME = 'chooser-cache-v1'
-const ASSETS_TO_CACHE = ['/', '/index.html']
+const CACHE_NAME = 'chooser-cache-v2'
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/apple-touch-icon.png',
+  '/favicon-32x32.png',
+  '/favicon-16x16.png',
+  '/site.webmanifest',
+]
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
